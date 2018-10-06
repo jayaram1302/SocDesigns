1d9e939509d7a33780d782672351b889e1713476
jayaram1302 <39757152+jayaram1302@users.noreply.github.com>
2018-10-06 12:20:30 +0530

    Create interface.sv

---
+++ PWM/interface.sv
@@ -0,0 +1,12 @@
+interface apb_if;
+   logic pclk;
+   logic presetn;
+   logic psel;
+   logic pwrite;
+   logic penable;
+   logic pready;
+   logic [31:0]paddr;
+   logic [31:0]pwdata;
+   logic [31:0]prdata;
+   logic pslverr;
+endinterface
